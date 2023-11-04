e3dd330f1c8ea8a7f6462282da573572b4b20267
June woo Kang <harry020104@naver.com>
2023-11-04 14:51:28 +0900

    Increasing Model Types
    Increasing Model Types
    & rename Model.py(random.py)

--- Get.py
+++ Get.py
@@ -13,11 +13,23 @@ Get_year, Get_month, Get_date, Get_selectedCity, Get_vselectedModel = sys.argv[1
 Get_selectedCity = '0' if Get_selectedCity == 'Seoul' else '1'
 
 # 모델 선택을 숫자로 변환
-if Get_vselectedModel == 'Random':
+if Get_vselectedModel == 'LightGBM':
     Get_vselectedModel = '1'
-elif Get_vselectedModel == 'Time_series':
+elif Get_vselectedModel == 'XGBOOST':
     Get_vselectedModel = '2'
+elif Get_vselectedModel == 'Random_Forest':
+    Get_vselectedModel = '3'
+elif Get_vselectedModel == 'Gradient_Boosting':
+    Get_vselectedModel = '4'
+elif Get_vselectedModel == 'Decision_Tree':
+    Get_vselectedModel = '5'
+elif Get_vselectedModel == 'Linear_Regression':
+    Get_vselectedModel = '6'
+elif Get_vselectedModel == 'MLP':
+    Get_vselectedModel = '7'
+else :
+    pass
 
 # Model\Random.py 스크립트 실행
-#subprocess.run(['python', 'Model\\Random.py', Get_year, Get_month, Get_date, Get_selectedCity, Get_vselectedModel])
-subprocess.run(['python', 'Scroll.py', Get_year, Get_month, Get_date, Get_selectedCity, Get_vselectedModel])
+subprocess.run(['python', 'Model\\Model.py', Get_year, Get_month, Get_date, Get_selectedCity, Get_vselectedModel])
+#subprocess.run(['python', 'Scroll.py', Get_year, Get_month, Get_date, Get_selectedCity, Get_vselectedModel])
