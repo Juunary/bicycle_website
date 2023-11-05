ac9db510fa6d682328738dac1f640e8070b194b8
Kim jae hyun <78593147+rich-hyun@users.noreply.github.com>
2023-11-05 15:01:13 +0900

    jaehyun commit

--- model.py
+++ model.py
@@ -4,10 +4,19 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
-from scroll import data_customerpick
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+import lightgbm as lgb
+import warnings
+warnings.filterwarnings("ignore")
 
 #데이터 값 받기
-Get_year, Get_month ,Get_date, Get_selectedCity, Get_selectedModel=data_customerpick()
+Get_year = int(input())
+Get_month = int(input())
+Get_date = int(input())
+Get_selectedCity=0
+Get_selectedModel=1
 
 data = "2023-10-27 서울 랜덤포레스트 서울(108) 2023-10-27 15.7 NULL 1.9 71.1 10.07 NULL 16.5"
 
@@ -35,15 +44,15 @@ def process_user_data(data):
 def process_and_encode_data(data, df):
     # 사용자 데이터 처리
     result_df = process_user_data(data)
-    
+
     # 연도, 월, 일 컬럼 추출
     result_df['year'] = Get_year
     result_df['mnth'] = Get_month
     result_df['day'] = Get_date
-    
+
     # 원-핫 인코딩 처리
     result_df = pd.get_dummies(result_df, columns=['day', 'year', 'mnth', 'season'])
-    
+
     # 기존 데이터에 존재하는 모든 컬럼을 포함하도록 처리 (누락된 컬럼은 0으로 채움)
     for col in df.columns:
         if col not in result_df.columns:
@@ -67,76 +76,88 @@ def process_and_encode_data(data, df):
 def load_and_process_data():
     # 데이터 로딩
     df = pd.read_csv('https://raw.githubusercontent.com/rich-hyun/Battle-of-the-Strongest-Statisticians/main/18-23%20Seoul_day.csv')
-    
+
     # 'date' 컬럼을 날짜 형식으로 변환하고 연, 월, 일 정보 추출
     df['date'] = pd.to_datetime(df['date'])
     df['year'] = df['date'].dt.year
     df['mnth'] = df['date'].dt.month
     df['day'] = df['date'].dt.day
-    
+
     # 필요한 컬럼 선택
     selected_columns = ['day', 'year', 'mnth', 'avg_tmp', 'day_p', 'avg_wind', 'avg_rhum', 't_sd', 'snow', 'avg_gtmp', 'season', 'CNT']
     df = df[selected_columns]
-    
+
     # 'CNT' 컬럼의 쉼표 제거하고 실수형으로 변환
     df['CNT'] = df['CNT'].str.replace(',', '').astype(float)
-    
+
     # 범주형 데이터 원-핫 인코딩 처리 (필요한 컬럼에 대해서만 실행)
     df = pd.get_dummies(df, columns=['day', 'year', 'mnth', 'season'])
-    
+
     # 결측치 처리 (평균값으로 대체)
     df = df.fillna(df.mean())
-    
+
     # X와 y 데이터 설정
     X = df.drop(['CNT'], axis=1).values.astype(np.float32)
     y = df['CNT'].values.astype(np.float32)
-    
+
     return X, y, df
 
-def train_and_predict_rf(X_train, X_test, y_train, y_test):    
-    # 최적의 하이퍼파라미터로 랜덤 포레스트 모델 생성 및 훈련
-    rf_model = RandomForestRegressor(n_estimators=300, max_features='sqrt', max_depth=20, random_state=42)
-    rf_model.fit(X_train, y_train)
-    
-    # 테스트 데이터에 대한 예측 수행
-    y_pred = rf_model.predict(X_test)  
-    # 사용자 데이터 처리
+# 모델 훈련 및 예측 함수
+def train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, model_name):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     result_df = process_and_encode_data(data, df)
-    
-    # 예측 수행
-    predicted_CNT = rf_model.predict(result_df.values.astype(np.float32))
-    
-    # 예측 결과를 반올림
+    predicted_CNT = model.predict(result_df.values.astype(np.float32))
     predicted_CNT_rounded = round(predicted_CNT[0])
-    
-    # 예측 결과 출력
-    print(f"Predicted CNT (Rounded): {predicted_CNT_rounded}")
+    print(f"{model_name} predicted_cnt(rounded): {predicted_CNT_rounded}")
 
-def train_and_predict_xgb(X_train, X_test, y_train, y_test):    
-    # XGBoost 모델 훈련
-    xg_model = xgb.XGBRegressor()
-    xg_model.fit(X_train, y_train)
-    
-    # 사용자 데이터 처리
-    result_df = process_and_encode_data(data, df)
-    
-    # 사용자 데이터에 대한 예측 수행
-    user_pred = xg_model.predict(result_df.values.astype(np.float32))
-    
-    # 예측값 반올림 후 출력
-    print(f"Rounded Predicted Count: {round(user_pred[0])}")
+# 모델별 훈련 및 예측 함수 정의
+def train_and_predict_lgbm(X_train, X_test, y_train, y_test, data, df):
+    model = lgb.LGBMRegressor(max_depth=30, random_state=42)
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "LightGBM")
 
-# 함수 호출
+def train_and_predict_xgboost(X_train, X_test, y_train, y_test, data, df):
+    model = xgb.XGBRegressor(learning_rate=0.2, max_depth=3, n_estimators=100, random_state=42)
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "XGBoost")
+
+def train_and_predict_rf(X_train, X_test, y_train, y_test, data, df):
+    model = RandomForestRegressor(n_estimators=300, max_features='sqrt', max_depth=20, random_state=42)
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "Random Forest")
+
+def train_and_predict_gb(X_train, X_test, y_train, y_test, data, df):
+    model = GradientBoostingRegressor(max_depth=4, n_estimators=200, random_state=42)
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "Gradient Boosting")
+
+def train_and_predict_dt(X_train, X_test, y_train, y_test, data, df):
+    model = DecisionTreeRegressor(max_depth=20, min_samples_split=10, random_state=42)
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "Decision Tree")
+
+def train_and_predict_lr(X_train, X_test, y_train, y_test, data, df):
+    model = LinearRegression()
+    train_and_predict_model(model, X_train, X_test, y_train, y_test, data, df, "Linear Regression")
+
+# 데이터 로딩 및 처리
 X, y, df = load_and_process_data()
-
-# 데이터를 훈련 세트와 테스트 세트로 분할
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-if Get_selectedModel==1:
-  # 함수 실행
-  train_and_predict_rf(X_train, X_test, y_train, y_test)
+# 모델 선택에 따라 해당 모델 훈련 및 예측
+if Get_selectedModel == 1:
+    train_and_predict_lgbm(X_train, X_test, y_train, y_test, data, df)
+
 elif Get_selectedModel == 2:
-  #함수 실행
-  train_and_predict_xgb(X_train, X_test, y_train, y_test)
+    train_and_predict_xgboost(X_train, X_test, y_train, y_test, data, df)
+
+elif Get_selectedModel == 3:
+    train_and_predict_rf(X_train, X_test, y_train, y_test, data, df)
+
+elif Get_selectedModel == 4:
+    train_and_predict_gb(X_train, X_test, y_train, y_test, data, df)
+
+elif Get_selectedModel == 5:
+    train_and_predict_dt(X_train, X_test, y_train, y_test, data, df)
+
+elif Get_selectedModel == 6:
+    train_and_predict_lr(X_train, X_test, y_train, y_test, data, df)
+
 else:
-    print("Invalid model selection. Please select 1 for RandomForest or 2 for XGBoost.")
+    print("error_wrong_number")
